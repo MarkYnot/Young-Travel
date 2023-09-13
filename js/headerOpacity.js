8d929d8a87e23fafb84d6787c20dd14d23053dba
MarkYnot <mark727221029@gmail.com>
2023-09-14 00:59:06 +1000

    remove opacity for mobile

--- js/headerOpacity.js
+++ js/headerOpacity.js
@@ -1,8 +1,10 @@
 const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", ()=>{
-  if(this.window.scrollY > navbar.offsetHeight){
+  if(this.window.scrollY > navbar.offsetHeight ){
+    if(window.innerWidth < 768) return
     navbar.style.backgroundColor = "rgba(255, 255, 255, 0.25)"
+   
 
   }else{
     navbar.style.backgroundColor = "white"
